f6a0ccb25723cdccdb92d69ca4d7b9ef2a2bceba
Michael Santos <michael_lsantos@yahoo.com.br>
2019-08-11 00:14:03 -0300

    Configuração de rotas e estilos da aplicação.

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="Meetapp" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta grátis</Link>
+      </form>
+    </>
+  );
+}
